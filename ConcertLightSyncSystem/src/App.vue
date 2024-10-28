f96f8f320cb05ae024971e99854ebfc6efd3751b
TomatoCakePasta <gaomuk50@gmail.com>
2024-10-28 23:18:54 +0900

    URLの調整

--- ConcertLightSyncSystem/src/App.vue
+++ ConcertLightSyncSystem/src/App.vue
@@ -5,7 +5,8 @@ import io from "socket.io-client";
 
 const PORT = 3000;
 
-const socket = io.connect(`http://localhost:${PORT}`);
+// const socket = io.connect(`http://localhost:${PORT}`);
+const socket = io.connect("https://concertlightsyncsystem.onrender.com:3000");
 
 const lightColor = ref("blue")
 
